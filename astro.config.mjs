5e456de6ae54b83c72d88c1a13ed9ff1b932c7c7
ViciiX <181275358@qq.com>
2026-07-07 00:38:44 +0800

    修复了文章底部链接错误的bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
+	site: "https://viciix.vercel.app/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
