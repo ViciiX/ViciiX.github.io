eb5993b6a494f5ddfcc432dcb73edb7f6136aa31
ViciiX <181275358@qq.com>
2026-07-06 23:54:51 +0800

    使用Astro构建的全新博客

---
+++ src/config.ts
@@ -0,0 +1,90 @@
+import type {
+	ExpressiveCodeConfig,
+	LicenseConfig,
+	NavBarConfig,
+	ProfileConfig,
+	SiteConfig,
+} from "./types/config";
+import { LinkPreset } from "./types/config";
+
+export const siteConfig: SiteConfig = {
+	title: "ViciiX 的 博客",
+	subtitle: "VcX's Blog",
+	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
+	themeColor: {
+		hue: 40, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+		fixed: true, // Hide the theme color picker for visitors
+	},
+	banner: {
+		enable: true,
+		src: "/images/website/banners/shizuku_leaf.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		position: "top", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
+		credit: {
+			enable: true, // Display the credit text of the banner image
+			text: "Tsukishima Ruriko - Shizuku - Leaf 1996", // Credit text to be displayed
+			url: "https://leaf.aquaplus.jp/product/sizuku/", // (Optional) URL link to the original artwork or artist's page
+		},
+	},
+	toc: {
+		enable: true, // Display the table of contents on the right side of the post
+		depth: 2, // Maximum heading depth to show in the table, from 1 to 3
+	},
+	favicon: [
+		// Leave this array empty to use the default favicon
+		// {
+		//   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
+		//   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+		//   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+		// }
+	],
+};
+
+export const navBarConfig: NavBarConfig = {
+	links: [
+		LinkPreset.Home,
+		LinkPreset.Archive,
+		LinkPreset.About,
+		{
+			name: "GitHub",
+			url: "https://github.com/ViciiX", // Internal links should not include the base path, as it is automatically added
+			external: true, // Show an external link icon and will open in a new tab
+		},
+	],
+};
+
+export const profileConfig: ProfileConfig = {
+	avatar: "/images/website/avatar_vcx.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	name: "ViciiX",
+	bio: "𝐆𝐢𝐯𝐞 𝐋𝐢𝐟𝐞 𝐁𝐚𝐜𝐤 𝐓𝐨 𝐌𝐮𝐬𝐢𝐜.",
+	links: [
+		{
+			name: "BiliBili",
+			icon: "fa6-brands:bilibili", // Visit https://icones.js.org/ for icon codes
+			// You will need to install the corresponding icon set if it's not already included
+			// `pnpm add @iconify-json/<icon-set-name>`
+			url: "https://space.bilibili.com/490054500",
+		},
+		{
+			name: "Steam",
+			icon: "fa6-brands:steam",
+			url: "https://steamcommunity.com/id/ViciiX/",
+		},
+		{
+			name: "GitHub",
+			icon: "fa6-brands:github",
+			url: "https://github.com/ViciiX",
+		},
+	],
+};
+
+export const licenseConfig: LicenseConfig = {
+	enable: true,
+	name: "CC BY-NC-SA 4.0",
+	url: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
+};
+
+export const expressiveCodeConfig: ExpressiveCodeConfig = {
+	// Note: Some styles (such as background color) are being overridden, see the astro.config.mjs file.
+	// Please select a dark theme, as this blog theme currently only supports dark background color
+	theme: "github-dark",
+};
---
+++ src/content/posts/draft.md
@@ -0,0 +1,14 @@
+---
+title: Hello, Astro!
+description: 哈哈哈哈哈
+published: 2026-07-06
+tags: []
+category: 博客
+draft: false
+---
+
+# <center>Hello, Astro!</center>
+今天终于把原来用Hexo搭建的博客重建了一下，换成了Astro，感觉确实是好用了很多  
+热更新太方便了！！  
+这个页面是临时的，得等我接下来想想要写点啥  
+=w=.  
